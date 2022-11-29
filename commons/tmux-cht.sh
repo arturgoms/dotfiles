2b4bc38bf8cfa05b58cc78ab9c2f002ca0fe8e18
Artur Gomes <contato@arturgomes.com.br>
2022-11-29 17:00:21 -0300

    Add commons scripts

---
+++ commons/tmux-cht.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+selected=`cat ~/.config/commons/.tmux-cht-languages ~/.config/commons/.tmux-cht-commands | fzf`
+if [[ -z $selected ]]; then
+    exit 0
+fi
+
+read -p "Enter Query: " query
+
+if grep -qs "$selected" ~/.tmux-cht-languages; then
+    echo 'is language'
+    query=`echo $query | tr ' ' '+'`
+    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+else
+    echo 'is command'
+    tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
+fi
