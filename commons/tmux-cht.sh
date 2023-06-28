01d4da7a4f872f11fa3eddda2e4deab103c7a892
Artur Gomes <contato@arturgomes.com.br>
2023-06-28 15:44:19 -0300

    tmux things

--- commons/tmux-cht.sh
+++ commons/tmux-cht.sh
@@ -12,5 +12,5 @@ if grep -qs "$selected" ~/.tmux-cht-languages; then
     tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
 else
     echo 'is command'
-    tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
+    tmux neww bash -c "curl -s cht.sh/$selected/$query | less"
 fi
